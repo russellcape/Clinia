b9a8c5e464c34ad7f983662eefca65c86a1632f5
Russell <russellcape98@gmail.com>
2020-11-23 14:42:02 -0500

    searches for the word through relative links

--- crawler.js
+++ crawler.js
@@ -2,18 +2,70 @@ const request = require('request');
 const cheerio = require('cheerio');
 const URL = require('url-parse');
 
-let pageToVisit = "http://www.google.com";
-console.log("Visiting page " + pageToVisit);
+const START_URL = "http://www.google.com";
+const SEARCH_WORD = "search";
+const MAX_PAGES_TO_VISIT = 10;
 
-request(pageToVisit, function(error, response, body) {
-    if (error) {
-        console.log("Error: " + error);
-    }
-    // Check status code (200 is HTTP OK)
-    console.log("Status code: " + response.statusCode);
-    if (response.statusCode === 200) {
-        // Parse the document body
-        const $ = cheerio.load(body);
-        console.log("Page title: " + $('title').text());
-    }
-})
+let pagesVisited = {};
+let numPagesVisited = 0;
+let pagesToVisit = [];
+let url = new URL(START_URL);
+const baseUrl = url.protocol + "//" + url.hostname;
+
+pagesToVisit.push(START_URL);
+crawl();
+
+function crawl() {
+  if(numPagesVisited >= MAX_PAGES_TO_VISIT) {
+    console.log("Reached max limit of number of pages to visit.");
+    return;
+  }
+  const nextPage = pagesToVisit.pop();
+  if (nextPage in pagesVisited) {
+    // We've already visited this page, so repeat the crawl
+    crawl();
+  } else {
+    // New page we haven't visited
+    visitPage(nextPage, crawl);
+  }
+}
+
+function visitPage(url, callback) {
+  // Add page to our set
+  pagesVisited[url] = true;
+  numPagesVisited++;
+
+  // Make the request
+  console.log("Visiting page " + url);
+  request(url, function(error, response, body) {
+     // Check status code (200 is HTTP OK)
+     console.log("Status code: " + response.statusCode);
+     if(response.statusCode !== 200) {
+       callback();
+       return;
+     }
+     // Parse the document body
+     const $ = cheerio.load(body);
+     const isWordFound = searchForWord($, SEARCH_WORD);
+     if(isWordFound) {
+       console.log('Word ' + SEARCH_WORD + ' found at page ' + url);
+     } else {
+       collectInternalLinks($);
+       // In this short program, our callback is just calling crawl()
+       callback();
+     }
+  });
+}
+
+function searchForWord($, word) {
+  const bodyText = $('html > body').text().toLowerCase();
+  return(bodyText.indexOf(word.toLowerCase()) !== -1);
+}
+
+function collectInternalLinks($) {
+    const relativeLinks = $("a[href^='/']");
+    console.log("Found " + relativeLinks.length + " relative links on page");
+    relativeLinks.each(function() {
+        pagesToVisit.push(baseUrl + $(this).attr('href'));
+    });
+}
