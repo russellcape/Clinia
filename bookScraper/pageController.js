3b48c2299e7271e0b3411a6ffbb28587007f0068
Russell <russellcape98@gmail.com>
2020-11-24 16:51:35 -0500

    Added fs package for saving data as a JSON

--- bookScraper/pageController.js
+++ bookScraper/pageController.js
@@ -1,4 +1,5 @@
 const pageScraper = require('./pageScraper');
+const fs = require('fs');
 
 async function scrapeAll(browserInstance) {
     let browser;
@@ -11,7 +12,12 @@ async function scrapeAll(browserInstance) {
         scrapedData['HistoricalFiction'] = await pageScraper.scraper(browser, 'Historical Fiction');
         scrapedData['Mystery'] = await pageScraper.scraper(browser, 'Mystery');
         await browser.close();
-        console.log(scrapedData)
+        fs.writeFile("data.json", JSON.stringify(scrapedData), 'utf8', function(err) {
+            if(err) {
+                return console.log(err);
+            }
+            console.log("The data has been scraped and saved successfully! View it at './data.json'");
+        });
     }
     catch(err) {
         console.log("Could not resolve the browser instance => ", err);
