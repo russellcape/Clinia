0697f76ae8fca71363db2a18857f95c3b9e15f6d
Russell <russellcape98@gmail.com>
2020-11-24 16:48:13 -0500

    Scraper can go through single or multiple pages searching multiple genres

--- bookScraper/pageController.js
+++ bookScraper/pageController.js
@@ -4,7 +4,14 @@ async function scrapeAll(browserInstance) {
     let browser;
     try {
         browser = await browserInstance;
-        await pageScraper.scraper(browser);
+        let scrapedData = {};
+
+        // Call the scraper for different set of books to be scraped
+        scrapedData['Travel'] = await pageScraper.scraper(browser, 'Travel');
+        scrapedData['HistoricalFiction'] = await pageScraper.scraper(browser, 'Historical Fiction');
+        scrapedData['Mystery'] = await pageScraper.scraper(browser, 'Mystery');
+        await browser.close();
+        console.log(scrapedData)
     }
     catch(err) {
         console.log("Could not resolve the browser instance => ", err);
